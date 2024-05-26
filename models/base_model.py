d8d0ecdbc9fda98fa27f1d5712c70d133482be43
Patrick <patricknwosu360@gmail.com>
2024-05-26 12:08:16 +0100

    new

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,8 @@
 import uuid
 from datetime import datetime
 """A module for the base model"""
+
+
 class BaseModel:
     """A class for the model"""
 
@@ -18,18 +20,20 @@ class BaseModel:
         super().__setattr__(name, value)
 
     def __str__(self):
-        """Prints a human readable string representation of object"""
+        """Prints a human readable
+        string representation of object"""
         return(f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
 
     def save(self):
-        """updates the public instance attribute updated_at with the current datetime"""
+        """updates the public instance attribute
+        updated_at with the current datetime"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__ of the instance"""
+        """returns a dictionary containing all
+        keys/values of __dict__ of the instance"""
         dict_repr = self.__dict__.copy()
         dict_repr['__class__'] = self.__class__.__name__
         dict_repr['created_at'] = self.created_at.isoformat()
         dict_repr['updated_at'] = self.updated_at.isoformat()
         return dict_repr
-        
