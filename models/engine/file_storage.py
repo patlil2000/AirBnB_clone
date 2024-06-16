fc7f2fede2dd76cc6b880790baf5b6398d2d6248
Patrick <patricknwosu360@gmail.com>
2024-06-16 01:01:49 +0100

    new

---
+++ models/engine/file_storage.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import json
+import os
+
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Returns the object"""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        objects_dict = {key: obj.to_dict()
+                        for key, obj in FileStorage.__objects.items()}
+        with open(FileStorage.__file_path, "w") as file:
+            json.dump(objects_dict, file)
+
+    def reload(self):
+        if os.path.exists(FileStorage.__file_path):
+            with open(FileStorage.__file_path, "r") as file:
+                data = json.load(file)
+                from models.base_model import BaseModel
+                FileStorage.__objects = {key: BaseModel(**value)
+                                        for key, value in data.items()}
+        else:
+            pass
